ecd3a7f51349fbc47e3ba0b8992d9c8478d0880e
Andravi <andravi75@gmail.com>
2025-05-06 18:41:16 -0300

    add juice and guarnicao

--- teste.js
+++ teste.js
@@ -4,6 +4,16 @@ const cheerio = require("cheerio");
 const url =
   "https://www.ufc.br/restaurante/cardapio/1-restaurante-universitario-de-fortaleza";
 
+let pratosAlmoco = [];
+let sucoAlmoco = "";
+let acompanhamentoAlmoco = "";
+let pratosJanta = [];
+let sucoJanta = "";
+let acompanhamentoJantar = "";
+
+const querCarioca = false; // pegar por argumentos no momento da chamada
+const querPure = false; // pegar por argumentos no momento da chamada
+
 function pushToList(list, item, index) {
   if (item.includes("(Contém")) {
     list[index - 1] = list[index - 1] + item;
@@ -18,8 +28,6 @@ axios
   .then((response) => {
     const $ = cheerio.load(response.data);
 
-    let pratosAlmoco = [];
-    let pratosJanta = [];
     $(".almoco .principal .desc, .almoco .vegetariano .desc").each(
       (index, element) => {
         let prato = $(element).text();
@@ -33,11 +41,37 @@ axios
         pushToList(pratosJanta, prato, index);
       }
     );
+
+    $(".almoco .suco .desc").each((index, element) => {
+      sucoAlmoco = $(element).text();
+    });
+
+    $(".jantar .suco .desc").each((index, element) => {
+      sucoJanta = $(element).text();
+    });
+
+    $(".almoco .guarnicao .desc").each((index, element) => {
+      acompanhamentoAlmoco = $(element).text();
+    });
+
+    $(".jantar .guarnicao .desc").each((index, element) => {
+      acompanhamentoJantar = $(element).text();
+    });
+
     pratosAlmoco = pratosAlmoco.filter((n) => n);
     pratosJanta = pratosJanta.filter((n) => n);
 
-    console.log("Pratos do Almoço:", pratosAlmoco);
-    console.log("Pratos da Janta:", pratosJanta);
+    console.log("ALMOÇO:");
+    console.log("Pratos:", pratosAlmoco);
+    console.log("Acompanhamento:", acompanhamentoAlmoco);
+    console.log("Suco de", sucoAlmoco);
+
+    console.log();
+
+    console.log("JANTAR:");
+    console.log("Pratos:", pratosJanta);
+    console.log("Acompanhamento:", acompanhamentoJantar);
+    console.log("Suco de", sucoJanta);
   })
   .catch((error) => {
     console.error("Erro ao acessar a página:", error.message);
